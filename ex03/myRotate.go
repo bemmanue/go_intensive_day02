5e7c4e059de4d994ef39d0b6263f458748c3451b
Burma Emmanuel <bemmanue@si-g4.kzn.21-school.ru>
2022-12-24 18:46:40 +0300

    fix creating archive

--- ex03/myRotate.go
+++ ex03/myRotate.go
@@ -15,58 +15,6 @@ import (
 	"time"
 )
 
-func addToArchive(tw *tar.Writer, filename string) error {
-	file, err := os.Open(filename)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	info, err := file.Stat()
-	if err != nil {
-		return err
-	}
-
-	header, err := tar.FileInfoHeader(info, info.Name())
-	if err != nil {
-		return err
-	}
-
-	err = tw.WriteHeader(header)
-	if err != nil {
-		return err
-	}
-
-	_, err = io.Copy(tw, file)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func createArchive(filename, path string, group *sync.WaitGroup) {
-	archname := getArchiveName(filename, path)
-
-	archive, err := os.Create(archname)
-	if err != nil {
-		log.Fatalln("Error writing archive:", err)
-	}
-	defer archive.Close()
-
-	gw := gzip.NewWriter(archive)
-	defer gw.Close()
-	tw := tar.NewWriter(gw)
-	defer tw.Close()
-
-	err = addToArchive(tw, filename)
-	if err != nil {
-		log.Fatalln("Error writing archive:", err)
-	}
-
-	group.Done()
-}
-
 func getArchiveName(filename, path string) string {
 	var name string
 	var timestamp string
@@ -86,6 +34,71 @@ func getArchiveName(filename, path string) string {
 	return name
 }
 
+func createArchive(filename string, out io.Writer) error {
+	gw := gzip.NewWriter(out)
+	defer gw.Close()
+	tw := tar.NewWriter(gw)
+	defer tw.Close()
+
+	err := addToArchive(tw, filename)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func addToArchive(tw *tar.Writer, filename string) error {
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	info, err := file.Stat()
+	if err != nil {
+		return err
+	}
+
+	header, err := tar.FileInfoHeader(info, info.Name())
+	if err != nil {
+		return err
+	}
+
+	header.Name = filename
+
+	err = tw.WriteHeader(header)
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(tw, file)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func archive(filename, path string, group *sync.WaitGroup) {
+	archname := getArchiveName(filename, path)
+
+	out, err := os.Create(archname)
+	if err != nil {
+		log.Fatalln("Error writing archive:", err)
+	}
+	defer out.Close()
+
+	err = createArchive(filename, out)
+	if err != nil {
+		log.Fatalln("Error creating archive:", err)
+	}
+
+	fmt.Println("Archive created successfully")
+
+	group.Done()
+}
+
 func main() {
 	a := flag.String("a", "", "put archive into specified directory")
 	flag.Parse()
@@ -94,14 +107,14 @@ func main() {
 		var group sync.WaitGroup
 		for i := 1; i < len(os.Args); i++ {
 			group.Add(1)
-			go createArchive(os.Args[i], *a, &group)
+			go archive(os.Args[i], *a, &group)
 		}
 		group.Wait()
 	} else if *a != "" && len(os.Args) >= 4 {
 		var group sync.WaitGroup
 		for i := 3; i < len(os.Args); i++ {
 			group.Add(1)
-			go createArchive(os.Args[i], *a, &group)
+			go archive(os.Args[i], *a, &group)
 		}
 		group.Wait()
 	} else {
